53dd7a44dc5a47daaa579defa17a8b59a7f6f933
rrodrigodlima <rdrigo019@gmail.com>
2022-10-28 12:22:35 -0300

    requisito 5 e 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,14 +30,33 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
+  const product1 = 'Alcool';
+  const product2 = 'Mascara';
+  const details = productDetails(product1, product2);
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
+    expect(typeof productDetails).toBe('function');
+  });
+  it('Teste se o retorno da função é um array.', () => {
+    expect(Array.isArray(productDetails())).toBe(true);
+    expect(Array.isArray(details)).toBe(true);
+  });
+  it('Teste se o array retornado pela função contém dois itens dentro.', () => {
+    expect(productDetails()).toHaveLength(2);
+    expect(details).toHaveLength(2);
+  });
+  it('Teste se os dois itens dentro do array retornado pela função são objetos.', () => {
+    expect(productDetails()[0]).toMatchObject({});
+    expect(productDetails()[1]).toMatchObject({});
+    expect(details[0]).toMatchObject({});
+    expect(details[1]).toMatchObject({});
+  });
+  it('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
+    expect(details[0]).not.toEqual(details[1]);
+  });
+  it('Teste se os dois productIds terminam com 123.', () => {
+    expect(details).toEqual(expect.arrayContaining([
+      expect.objectContaining({details: {productId: product1+'123'}}),
+      expect.objectContaining({details: {productId: product2+'123'}}),
+    ]))
   });
 });
